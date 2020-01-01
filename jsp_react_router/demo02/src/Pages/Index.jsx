05261a49d2532c39bd0a37ad90e37a5a4d289917
sihuanian <2267322300@qq.com>
2020-01-02 00:17:01 +0800

    react router

---
+++ jsp_react_router/demo02/src/Pages/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Index() {
+  return(
+    <h3>我是首页</h3>
+  )
+}
+
+export default Index
---
+++ jsp_react_router/demo02/src/Pages/WorkPlace/GetUp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <h2>GET UP</h2>
+  )
+}
